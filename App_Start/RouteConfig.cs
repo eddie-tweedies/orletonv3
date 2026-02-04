38a35ca011c8239a438947f191ccaea14dcb08b6
Eddie Clanzy-Hodge <eddietweedies@outlook.com>
2026-02-04 15:05:14 +0000

    after restore

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,34 +1,18 @@
-using Microsoft.AspNet.FriendlyUrls;
 using System;
+using System.Collections.Generic;
+using System.Web;
 using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
 
 namespace orleton
 {
     public static class RouteConfig
-
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            if (routes == null)
-            {
-                throw new ArgumentNullException(nameof(routes), "Route collection cannot be null.");
-            }
-
-            var settings = new FriendlyUrlSettings
-            {
-                AutoRedirectMode = RedirectMode.Permanent
-            };
-
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
             routes.EnableFriendlyUrls(settings);
         }
     }
-
-    //{
-    //    public static void RegisterRoutes(RouteCollection routes)
-    //    {
-    //        var settings = new FriendlyUrlSettings();
-    //        settings.AutoRedirectMode = RedirectMode.Permanent;
-    //        routes.EnableFriendlyUrls(settings);
-    //    }
-    //}
 }
